d09ddea8b679e8066410b8591f71192b2dbe1ac3
Negar Kamvij <negarkamvizh@gmail.com>
2025-09-23 16:13:27 -0400

    Fix renderBoard typo and test X/O display

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("Hello World");
+
+let board = [
+        ["X", "O", "X"],
+        ["O", "X", "O"],
+        ["X", "O", "X"],
+    ];
+let currentPlayer = "X";
+let gameOver = false;
+let winner = null;    
+
+function initializeGame() {
+    board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ];
+}
+function renderBoard(){
+    const gameBoard = document.getElementById("gameBoard");
+    const rows = gameBoard.getElementsByClassName("row");
+
+    for (let i =0; i<3; i++){
+        const cells = rows[i].getElementsByClassName("cell");
+        for (let j=0; j<3; j++){
+            cells[j].innerText = board[i][j];
+        }
+       }
+}
+renderBoard();
