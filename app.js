8f16fdc2a954a3999e6e36d405501a2ea0d50d1f
Negar Kamvij <negarkamvizh@gmail.com>
2025-09-23 16:32:48 -0400

    DebugnrenderBoard logging

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 console.log("Hello World");
 
-let board = [
-        ["X", "O", "X"],
-        ["O", "X", "O"],
-        ["X", "O", "X"],
-    ];
+
 let currentPlayer = "X";
 let gameOver = false;
 let winner = null;    
 
-function initializeGame() {
+/*function initializeGame() {
     board = [
         ["", "", ""],
         ["", "", ""],
         ["", "", ""],
     ];
-}
+}*/
+let board = [
+        ["X", "O", "X"],
+        ["O", "X", "O"],
+        ["X", "O", "X"],
+    ];
 function renderBoard(){
     const gameBoard = document.getElementById("gameBoard");
     const rows = gameBoard.getElementsByClassName("row");
@@ -23,6 +24,7 @@ function renderBoard(){
     for (let i =0; i<3; i++){
         const cells = rows[i].getElementsByClassName("cell");
         for (let j=0; j<3; j++){
+            console.log(`Updating cell ${i},${j} to`, board[i][j]);
             cells[j].innerText = board[i][j];
         }
        }
